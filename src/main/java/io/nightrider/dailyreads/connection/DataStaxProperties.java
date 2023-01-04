22474c8c3fe289fc338cabe2c0cd9ea592170dfb
healingfields <dihratene@gmail.com>
2023-01-05 00:12:52 +0100

    created the config file for cassandra

---
+++ src/main/java/io/nightrider/dailyreads/connection/DataStaxProperties.java
@@ -0,0 +1,23 @@
+package io.nightrider.dailyreads.connection;
+
+import java.io.File;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "datastax.astra")
+public class DataStaxProperties {
+
+    private File secureConnectBundle;
+
+    public File getSecureConnectBundle() {
+        return secureConnectBundle;
+    }
+
+    public void setSecureConnectBundle(File secureConnectBundle) {
+        this.secureConnectBundle = secureConnectBundle;
+    }
+
+}
