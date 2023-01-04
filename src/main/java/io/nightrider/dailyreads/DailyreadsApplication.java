2eb3c6988a1f34e1297a2c1cbae71de9b2795d9e
healingfields <dihratene@gmail.com>
2023-01-05 00:13:34 +0100

    created the cqlSessionBuilderCustomizer bean, established the connection

--- src/main/java/io/nightrider/dailyreads/DailyreadsApplication.java
+++ src/main/java/io/nightrider/dailyreads/DailyreadsApplication.java
@@ -1,13 +1,25 @@
 package io.nightrider.dailyreads;
 
+import java.nio.file.Path;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+
+import io.nightrider.dailyreads.connection.DataStaxProperties;
 
 @SpringBootApplication
 public class DailyreadsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DailyreadsApplication.class, args);
+	}
+
+	@Bean
+	public CqlSessionBuilderCustomizer cqlSessionBuilderCustomizer(DataStaxProperties dataStaxProperties){
+		Path bundle = dataStaxProperties.getSecureConnectBundle().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(bundle);
 	}
 
 }
