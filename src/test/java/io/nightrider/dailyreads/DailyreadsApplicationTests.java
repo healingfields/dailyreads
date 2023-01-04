9a425c493e4ad1565234cfedb8a9cf2b3477d62d
healingfields <dihratene@gmail.com>
2023-01-05 00:04:50 +0100

    bootstraped the project

---
+++ src/test/java/io/nightrider/dailyreads/DailyreadsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nightrider.dailyreads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyreadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
